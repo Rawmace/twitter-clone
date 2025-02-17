77b1d18ce925064200822a226e01668b5beea0cb
Ramesh   Devkota <drhsemar@gmail.com>
2025-02-17 15:49:29 -0500

    Enhance Post component with like, comment, and delete functionalities; implemented loading states and error handling; add date formatting utility

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middleware to parse JSON requests
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 

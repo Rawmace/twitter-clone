aef61a45c3e5798954f2cc711b8cad9ba2b197fa
Ramesh   Devkota <drhsemar@gmail.com>
2025-02-21 16:00:09 -0500

    minor change in server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ cloudinary.config({
 // Define and declare constants
 const PORT = process.env.PORT || 5000;
 const app = express();
-const __dirname = path.reslove();
+const __dirname = path.resolve();
 
 // Middleware to parse JSON requests
 app.use(express.json({ limit: "5mb" }));

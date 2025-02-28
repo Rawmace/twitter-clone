148d2a9003c52bb7b59f860a4cb81ecfd52a3515
Ramesh   Devkota <drhsemar@gmail.com>
2025-02-27 19:34:58 -0500

    Server.js -- apisecret -- env variable

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret1: process.env,
 });
 // Define and declare constants
 const PORT = process.env.PORT || 5000;
